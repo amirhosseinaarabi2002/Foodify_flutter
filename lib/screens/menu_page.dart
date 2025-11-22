95d5a0612cd4f8e1e688ee711c29ba3cfe6c9bf0
amirhosseinaarabi2002 <amiraarabisw2002@gmail.com>
2025-11-22 04:33:34 -0800

    foodify: add types and also bottom navigation bar and floating action button

--- lib/screens/menu_page.dart
+++ lib/screens/menu_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodify/const/constants.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -8,8 +9,95 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
+  int selectedIndex = 0;
+
+  final List<String> _types = [
+    "|Recommended |",
+    "|Most Popular |",
+    "|Specials |",
+    "|High Demand |",
+    "|Limited |",
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('menu page')));
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Search
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                width: size.width * 0.9,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Constants.primaryColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.search, color: Colors.grey),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        cursorColor: Constants.primaryColor,
+                        decoration: InputDecoration(
+                          hintText: "Search...",
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(
+                          fontFamily: "QuickMedium",
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Icon(Icons.mic, color: Colors.grey),
+                  ],
+                ),
+              ),
+            ),
+            // Category
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              height: 70,
+              width: size.width,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Text(
+                        _types[index],
+                        style: TextStyle(
+                          fontFamily: "QuickMedium",
+                          fontSize: 16,
+                          fontWeight: selectedIndex == index
+                              ? FontWeight.bold
+                              : FontWeight.w300,
+                          color: selectedIndex == index
+                              ? Constants.primaryColor
+                              : Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: _types.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/root_page.dart
+++ lib/screens/root_page.dart
@@ -1,4 +1,12 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:foodify/const/constants.dart';
+import 'package:foodify/screens/cart_page.dart';
+import 'package:foodify/screens/favorite_page.dart';
+import 'package:foodify/screens/menu_page.dart';
+import 'package:foodify/screens/profile_page.dart';
+import 'package:foodify/screens/scan_page.dart';
+import 'package:page_transition/page_transition.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -8,8 +16,66 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  int bottomIndex = 0;
+
+  List<Widget> page = [MenuPage(), FavoritePage(), CartPage(), ProfilePage()];
+
+  List<IconData> iconList = [
+    Icons.home,
+    Icons.favorite,
+    Icons.shopping_cart,
+    Icons.person,
+  ];
+
+  List<String> appbarTitle = ["Home", "Favorites", "Basket", "Profile"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('root page')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                appbarTitle[bottomIndex],
+                style: TextStyle(color: Colors.grey, fontFamily: "QuickBold"),
+              ),
+              Icon(Icons.notifications, color: Colors.grey),
+            ],
+          ),
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+      ),
+      body: IndexedStack(index: bottomIndex, children: page),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            PageTransition(
+              type: PageTransitionType.leftToRight,
+              child: ScanPage(),
+            ),
+          );
+        },
+        backgroundColor: Constants.primaryColor,
+        shape: CircleBorder(),
+        child: Image.asset("assets/images/Scan.png", height: 30),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: bottomIndex,
+        onTap: (index) => setState(() {
+          bottomIndex = index;
+        }),
+        splashColor: Constants.primaryColor,
+        activeColor: Constants.primaryColor,
+        inactiveColor: Colors.grey,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+      ),
+    );
   }
 }
